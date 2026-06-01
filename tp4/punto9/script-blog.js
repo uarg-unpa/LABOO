f9776ab17989b205400b479caa093a3461fe1905
Axel <axel.cruz14@gmail.com>
2026-06-01 19:35:46 -0300

    Imágenes

--- tp4/punto9/script-blog.js
+++ tp4/punto9/script-blog.js
@@ -1,16 +1,19 @@
-// Nos aseguramos que el HTML haya cargado completamente
-window.addEventListener('load', function() {
+// Asegurar que el HTML se haya cargado completamente
+
+window.addEventListener('load', function() {    
     
-    // ==========================================
     // FUNCIÓN 1: Activar/Desactivar Modo Oscuro
-    // ==========================================
+    
     const btnModoOscuro = document.getElementById('btnModoOscuro');
     
     btnModoOscuro.addEventListener('click', function() {
+    
         // La propiedad 'toggle' agrega la clase si no está, o la quita si ya está
+    
         document.body.classList.toggle('modo-oscuro');
         
         // Cambiamos el texto del botón según el estado
+    
         if (document.body.classList.contains('modo-oscuro')) {
             btnModoOscuro.textContent = "Desactivar Modo Oscuro";
         } else {
@@ -18,19 +21,18 @@ window.addEventListener('load', function() {
         }
     });
 
-    // ==========================================
     // FUNCIÓN 2: Contar la cantidad de artículos
-    // ==========================================
+    
     const btnContar = document.getElementById('btnContar');
     
     btnContar.addEventListener('click', function() {
-        // Usamos document.querySelectorAll para buscar todos los <article> de la clase 'articulo-blog'
+        // Usar document.querySelectorAll para buscar todos los <article> de la clase 'articulo-blog'
         const articulos = document.querySelectorAll('.articulo-blog');
         
-        // Contamos cuántos elementos encontró
+        // Contar cuántos elementos encontró
         const cantidad = articulos.length;
         
-        // Mostramos el resultado en una ventana emergente
+        // Mostrar el resultado en una ventana emergente
         alert("El blog actualmente cuenta con " + cantidad + " artículos publicados.");
     });
 
